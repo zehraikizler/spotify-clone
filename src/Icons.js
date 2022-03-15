6b7b55a4708494d22b6230ea5f0f4316c3ae13da
Zehra ikizler <zehraaikizler@gmail.com>
2022-03-16 00:41:21 +0300

    Auth finished

--- src/Icons.js
+++ src/Icons.js
@@ -62,6 +62,22 @@ const NextIcon = ({size}) => {
     )
 }
 
+const DownDirIcon = ({size}) => {
+    return (
+        <svg role="img" height={size} width={size} class="Svg-sc-1bi12j5-0 hDgDGI uTkQAMWL5SHXimdorVLl" viewBox="0 0 16 16">
+            <path d="M14 6l-6 6-6-6h12z" fill="currentColor"></path>
+        </svg>
+    )
+}
+
+const ExternalIcon = ({size}) => {
+    return (
+        <svg role="img" height={size} width={size} viewBox="0 0 16 16" class="Svg-sc-1bi12j5-0 hDgDGI">
+            <path d="M1 2.75A.75.75 0 011.75 2H7v1.5H2.5v11h10.219V9h1.5v6.25a.75.75 0 01-.75.75H1.75a.75.75 0 01-.75-.75V2.75z" fill="currentColor"></path><path d="M15 1v4.993a.75.75 0 11-1.5 0V3.56L8.78 8.28a.75.75 0 01-1.06-1.06l4.72-4.72h-2.433a.75.75 0 010-1.5H15z" fill="currentColor"></path>
+        </svg>
+    )
+}
+
 const Icon = ({ name, size = 24 }) => {
 
     const icons = {
@@ -72,7 +88,9 @@ const Icon = ({ name, size = 24 }) => {
         heart: HeartIcon,
         download: DownloadIcon,
         prev: PrevIcon,
-        next: NextIcon
+        next: NextIcon,
+        downDir: DownDirIcon,
+        external: ExternalIcon
     }
 
     const Component = icons[name]
--- src/components/Navbar/Auth.js
+++ src/components/Navbar/Auth.js
@@ -1,7 +1,114 @@
-import React from 'react'
+import { Menu } from "@headlessui/react";
+import { Icon } from "Icons";
 
 export default function Auth() {
+  const user = {
+    name: "Zehra İkizler",
+    photo: "https://avatars.githubusercontent.com/u/79922791?v=4",
+  };
+
   return (
-    <div>Auth</div>
-  )
+    <Menu as="nav" className={"relative"}>
+      {({ open }) => (
+        <>
+          <Menu.Button
+            className={`flex items-center h-8 rounded-3xl pr-2 ${
+              open ? "bg-active" : "bg-black"
+            } hover:bg-active`}
+          >
+            <img
+              src={user.photo}
+              className={"w-8 h-8 rounded-full p-0.5 mr-2"}
+            />
+            <span className="text-sm font-semibold mr-2">{user.name}</span>
+            <span className={open === true && "rotate-180"}>
+              <Icon size={16} name="downDir" />
+            </span>
+          </Menu.Button>
+          <Menu.Items
+            className={
+              "absolute p-1 top-full right-0 w-48 bg-active rounded translate-y-2"
+            }
+          >
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`h-10 flex justify-between items-center px-2 text-sm rounded ${
+                    active && "bg-white bg-opacity-10"
+                  }`}
+                  href="#"
+                >
+                  Hesap
+                  <Icon size={16} name="external" />
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`h-10 flex justify-between items-center px-2 text-sm rounded ${
+                    active && "bg-white bg-opacity-10"
+                  }`}
+                  href="#"
+                >
+                  Profil
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`h-10 flex justify-between items-center px-2 text-sm rounded ${
+                    active && "bg-white bg-opacity-10"
+                  }`}
+                  href="#"
+                >
+                  Premium
+                  <Icon size={16} name="external" />
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`h-10 flex justify-between items-center px-2 text-sm rounded ${
+                    active && "bg-white bg-opacity-10"
+                  }`}
+                  href="#"
+                >
+                  Destek
+                  <Icon size={16} name="external" />
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`h-10 flex justify-between items-center px-2 text-sm rounded ${
+                    active && "bg-white bg-opacity-10"
+                  }`}
+                  href="#"
+                >
+                  İndir
+                  <Icon size={16} name="external" />
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`h-10 flex justify-between items-center px-2 text-sm rounded ${
+                    active && "bg-white bg-opacity-10"
+                  }`}
+                  href="#"
+                >
+                  Oturumu kapat
+                </a>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </>
+      )}
+    </Menu>
+  );
 }
